b3b3740394b322258e59e5d4026e7862e287e878
Sebastiaan Deckers <cbas@pandion.be>
2010-10-12 20:22:59 +0800

    Refactoring core files

--- core/app.js
+++ core/app.js
@@ -3,7 +3,7 @@
 	@author Copyright (c) 2010 Sebastiaan Deckers
 	@license GNU General Public License version 3 or later
 */
-require.def(["modules/events", "modules/loader"], function (events, settings) {
+require.def(["core/events", "core/loader"], function (events, settings) {
 	events.publish("app.ready");
 	return {};
 });
