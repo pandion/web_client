62081772f64012b5cd16042964e7876693abf3ae
Sebastiaan Deckers <cbas@pandion.be>
2010-10-26 16:44:32 +0800

    Starting work on the polyfill compatibility layer

---
+++ libraries/HTMLElement.js
@@ -0,0 +1,59 @@
+﻿/**
+	This file is part of Web Client
+	@author Copyright (c) 2010 Sebastiaan Deckers
+	@license GNU General Public License version 3 or later
+*/
+
+// Making the HTMLElement.prototype writable in all browsers.
+// http://my.opera.com/_Grey_/blog/2007/04/21/safari-and-htmlelement-prototype
+(function () {
+	var w = window;
+	if (!w.HTMLElement && (typeof document.createElement) === "function" // check basics
+		&& (var t = document.createElement("a").__proto__) // '__proto__' supported?
+		&& t == document.createElement('p').__proto__ // all HTMLElements the same?
+	) {
+		w.HTMLElement = {}; // prevent people from constructing
+		w.HTMLElement.prototype = t;
+	}
+})();
+
+// Based on http://forums.mozillazine.org/viewtopic.php?f=25&t=445587
+if (HTMLElement && !("insertAdjacentElement" in HTMLElement.prototype)) {
+	HTMLElement.prototype.insertAdjacentElement = function (position, element) {
+		switch (position) {
+			case "beforeBegin":
+				this.parentNode.insertBefore(element, this);
+				break;
+			case "afterBegin":
+				this.insertBefore(element, this.firstChild);
+				break;
+			case "beforeEnd":
+				this.appendChild(element);
+				break;
+			case "afterEnd":
+				if (this.nextSibling) {
+					this.parentNode.insertBefore(element, this.nextSibling);
+				} else {
+					this.parentNode.appendChild(element);
+				}
+				break;
+		}
+	};
+}
+
+// Based on http://forums.mozillazine.org/viewtopic.php?f=25&t=445587
+if (HTMLElement && !("insertAdjacentHTML" in HTMLElement.prototype)) {
+	HTMLElement.prototype.insertAdjacentHTML = function (position, html) {
+		var range = this.ownerDocument.createRange();
+		range.setStartBefore(this);
+		var fragment = range.createContextualFragment(html);
+		this.insertAdjacentElement(position, fragment);
+	};
+}
+
+// Based on http://forums.mozillazine.org/viewtopic.php?f=25&t=445587
+if (HTMLElement && !("insertAdjacentText" in HTMLElement.prototype)) {
+	HTMLElement.prototype.insertAdjacentText = function (position, text) {
+		this.insertAdjacentElement(position, document.createTextNode(text));
+	};
+}
---
+++ libraries/polyfill.js
@@ -0,0 +1,17 @@
+﻿/**
+	This file is part of Web Client
+	@author Copyright (c) 2010 Sebastiaan Deckers
+	@license GNU General Public License version 3 or later
+*/
+define(
+	[
+		"libraries/global-es5.js",
+		"libraries/json2.js",
+		"libraries/XMLHttpRequest.js",
+		"libraries/HTMLElement.js",
+		"libraries/classList.js"
+	],
+	function () {
+		return {};
+	}
+);
