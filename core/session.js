ed3197d855949f3e83c23f82962f32edcde6340a
Sebastiaan Deckers <cbas@pandion.be>
2010-10-27 09:41:06 +0800

    Using Mustache templates to abstract HTML generation out of the JS code.

--- core/session.js
+++ core/session.js
@@ -3,7 +3,7 @@
 	@author Copyright (c) 2010 Sebastiaan Deckers
 	@license GNU General Public License version 3 or later
 */
-define(["core/events", "core/ui"], function (events, ui) {
+define(["core/events", "core/ui", "libraries/mustache", "text!templates/signin.mustache"], function (events, ui, mustache, signinTemplate) {
 	events.subscribe("app.ready", function () {
 		ui.addNavigation({
 			title: "Sign Out",
@@ -12,38 +12,10 @@ define(["core/events", "core/ui"], function (events, ui) {
 				location.reload();
 			}
 		});
-
-		var overlay = document.createElement("section");
-		overlay.id = "overlay";
-		var form = document.createElement("form");
-
-		var addressLabel = document.createElement("label");
-		addressLabel.textContent = "IM Address";
-		addressLabel.htmlFor = "signin-address";
-		form.appendChild(addressLabel);
-
-		var address = document.createElement("input");
-		address.type = "email";
-		address.id = "signin-address";
-		form.appendChild(address);
-
-		var passwordLabel = document.createElement("label");
-		passwordLabel.textContent = "Password";
-		passwordLabel.htmlFor = "signin-password";
-		form.appendChild(passwordLabel);
-
-		var password = document.createElement("input");
-		password.type = "password";
-		password.id = "signin-password";
-		form.appendChild(password);
-
-		var submit = document.createElement("input");
-		submit.type = "submit";
-		submit.value = "Sign In";
-		form.appendChild(submit);
-
-		overlay.appendChild(form);
-		document.body.appendChild(overlay);
+		document.body.insertAdjacentHTML("beforeEnd", mustache.to_html(signinTemplate));
+		document.querySelector("#signin form").addEventListener("submit", function (event) {
+			event.preventDefault();
+		}, false);
 	});
 	return {};
 });
---
+++ libraries/mustache.js
@@ -0,0 +1,8 @@
+﻿/**
+	This file is part of Web Client
+	@author Copyright (c) 2010 Sebastiaan Deckers
+	@license GNU General Public License version 3 or later
+*/
+define(["libraries/mustache_.js"], function () {
+	return Mustache;
+});
