e03797e879f0031410e12afe0b15e19a3ef46214
Sebastiaan Deckers <cbas@pandion.be>
2010-11-01 01:32:44 +0800

    Ignore external tampering with "undefined"

--- libraries/console.js
+++ libraries/console.js
@@ -3,8 +3,8 @@
 	@author Copyright (c) 2010 Sebastiaan Deckers
 	@license GNU General Public License version 3 or later
 */
-(function (_undefined) {
-	if (window.console === _undefined) {
+(function (undefined) {
+	if (window.console === undefined) {
 		window.console = {};
 		(	"assert count debug dir dirxml error group groupCollapsed groupEnd " +
 			"info log markTimeline profile profileEnd time timeEnd trace warn"
