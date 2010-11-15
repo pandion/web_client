1771c5b6189322fbbb6a17d9496fdbdbfb3ad25b
Sebastiaan Deckers <cbas@pandion.be>
2010-11-15 15:04:31 +0800

    - Changed JSONP call - Updated RequireJS

--- modules/twitterSearch.js
+++ modules/twitterSearch.js
@@ -5,9 +5,10 @@
 */
 define(["core/events"], function (events) {
 	events.subscribe("search.query", function (queryString) {
-		// http://search.twitter.com/search.json?q=123&callback=?
-		// http://search.twitter.com/trends/current.json?callback=?
-		require(["jsonp!http://search.twitter.com/search.json?q=" + encodeURIComponent(queryString) + "&callback=?"],
+		/* TODO: Don't use require.js for this JSONP call since it doesn't clean up the script tag and caches results.
+		   See: http://requirejs.org/docs/api.html#jsonp
+		 */
+		require(["http://search.twitter.com/search.json?q=" + encodeURIComponent(queryString) + "&callback=define"],
 			function (payload) {
 				events.publish("search.results", payload);
 			}
